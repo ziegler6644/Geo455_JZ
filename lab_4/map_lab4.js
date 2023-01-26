7cd70ed9c4584a21fc9003951bff72e5f00b2e8e
ziegler6644 <123512475+ziegler6644@users.noreply.github.com>
2023-01-26 14:24:12 -0600

    Add files via upload

---
+++ lab_4/map_lab4.js
@@ -0,0 +1,22 @@
+// Create custom popups with images
+
+
+
+
+// Set up landmark variables and individual landmarks
+
+
+
+//Set up the baselayers and WMS layer
+
+
+
+//Create the map variable
+
+
+
+// Create menu items
+
+
+//Create the menu
+ 
