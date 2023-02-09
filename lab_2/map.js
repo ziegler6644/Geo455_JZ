01c7dbd5594256168814e08a89435f79fb981a9d
ziegler6644 <123512475+ziegler6644@users.noreply.github.com>
2023-02-09 15:14:56 -0600

    Add files via upload

---
+++ lab_2/map.js
@@ -0,0 +1,15 @@
+var mymap = L.map("map").setView([43.81642117539796, -91.23209456992531], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+L.marker([43.81642117539796, -91.23209456992531])
+    .addTo(mymap)
+    .bindPopup("<b>Hello!</b><br>I am University of Wisconsin La Crosse.")
+    .openPopup();
